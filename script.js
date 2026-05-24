529f4ebe2ed8ff78410270ce7c73c1448f3c89dc
Aira Estorninos <airaestorninos@gmail.com>
2026-05-24 11:26:38 +0000

    fix  music name

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const bgMusic = new Audio('I Don\'t Want to Set the World on Fire (Fallout Lofi).mp3');
+const bgMusic = new Audio('falloutfire.mp3');
 bgMusic.loop = true;
 bgMusic.volume = 0.2;
 
-document.addEventListener('click', () => {
-    bgMusic.play().catch(e => console.log("User interaction required for music."));
-}, { once: true });
-
 document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('click', () => {
+        bgMusic.play().catch(e => console.log("Music play blocked:", e));
+    }, { once: true });
 
   // --- Initial States and Core Configurations ---
   let currentMachine = 'DFA';
